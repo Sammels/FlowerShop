23ecf13d4316fe7ef3ed09e4f70701df0e996d81
Weffy61 <weffy@mail.ru>
2024-03-11 22:26:52 +0300

    Html, styles

--- flower_app/views.py
+++ flower_app/views.py
@@ -2,28 +2,12 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'FlowerApp/index.html')
 
-def card(request):
-    return render(request, 'card.html')
-
-def catalog(request):
-    return render(request, 'catalog.html')
-
-def consultation(request):
-    return render(request, 'consultation.html')
-
-def order_step(request):
-    return render(request, 'order-step.html')
 
 def order(request):
-    return render(request, 'order.html')
+    return render(request, 'FlowerApp/order.html')
 
-def quiz_step(request):
-    return render(request, 'quiz-step.html')
 
-def quiz(request):
-    return render(request, 'quiz.html')
-
-def result(request):
-    return render(request, 'result.html')
+def catalog(request):
+    return render(request, 'FlowerApp/catalog.html')
--- flower_shop/urls.py
+++ flower_shop/urls.py
@@ -1,18 +1,13 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
-from flower_app import views
+from django.urls import path, include
 
 urlpatterns = [
-    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
-    path('card/', views.card, name='card'),
-    path('catalog/', views.catalog, name='catalog'),
-    path('consultation/', views.consultation, name='consultation'),
-    path('order-step/', views.order_step, name='order-step'),
-    path('order/', views.order, name='order'),
-    path('quiz-step/', views.quiz_step, name='quiz-step'),
-    path('quiz/', views.quiz, name='quiz'),
-    path('result/', views.result, name='result'),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('', include('FlowerApp.urls')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
